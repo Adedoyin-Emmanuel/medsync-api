c78e038dd4e970d896c4e444172b4af2c23bae45
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-13 23:57:01 +0100

    Feat: implemented auth middleware

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,7 +1,31 @@
 import { NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { response, logger } from "../utils";
+import { response } from "../utils";
 
-const useAuth = (req: any, res: any, next: NextFunction) => {};
+const useAuth = (req: any, res: any, next: NextFunction) => {
+  const refreshTokenCookie = req.cookies.refreshToken;
+
+  if (!refreshTokenCookie)
+    return response(res, 401, "Access denied, invalid or expired token");
+
+  const JWT_SECRET: any = process.env.JWT_PRIVATE_KEY;
+
+  let decodeCookie: any = jwt.verify(refreshTokenCookie, JWT_SECRET);
+
+  if (decodeCookie) {
+    req.user = decodeCookie;
+    if (decodeCookie.role == "user") {
+      req.userType = "user";
+      next();
+    } else if (decodeCookie.role == "hospital") {
+      req.userType = "hospital";
+      next();
+    } else {
+      return response(res, 401, "Invalid auth token");
+    }
+  } else {
+    return response(res, 401, "Invalid auth token");
+  }
+};
 
 export default useAuth;
