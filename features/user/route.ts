812e938325fb5c61b6387b35335ab80404325701
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-14 02:50:02 +0100

    Feat: implemented user routes:

---
+++ features/user/route.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import {
+  useAuth,
+  upload,
+  fileUpload,
+  handleFileSizeLimitException,
+  useCheckRole,
+} from "../../middlewares/";
+import UserController from "./controller";
+const userRouter = express.Router();
+
+userRouter.get("/me", [useAuth], UserController.getMe);
+
+userRouter.get("/", [useAuth], UserController.getAllUsers);
+userRouter.get("/hospital", [useAuth], UserController.getAllHospitals);
+
+userRouter.get("/:id", UserController.getEntityById);
+
+userRouter.put("/", [useAuth, useCheckRole("user")], UserController.updateUser);
+userRouter.put(
+  "/hospital/",
+  [
+    useAuth,
+    useCheckRole("hospital"),
+    handleFileSizeLimitException,
+    upload.fields([{ name: "profilePicture", maxCount: 1 }]),
+    fileUpload,
+  ],
+  UserController.updateHospital
+);
+
+export default userRouter;
