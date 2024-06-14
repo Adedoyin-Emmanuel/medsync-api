824af1d6d481d3b87c06c314ce2cc22593be671e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-14 02:57:15 +0100

    Feat: add medicaition validation schema

---
+++ features/medication/schema.ts
@@ -0,0 +1,27 @@
+import Joi from "joi";
+
+export const createMedicationSchema = Joi.object({
+  userId: Joi.string().required(),
+  name: Joi.string().required().max(250),
+  description: Joi.string().required().max(2500),
+  dosage: Joi.string().required().max(2500),
+  totalPrice: Joi.number().required(),
+  drugs: Joi.array().items().required(),
+  sideEffects: Joi.array().items().optional(),
+});
+
+export const updateMedicationSchema = Joi.object({
+  name: Joi.string().optional().max(250),
+  description: Joi.string().optional().max(2500),
+  dosage: Joi.string().optional().max(2500),
+  totalPrice: Joi.number().optional(),
+  drugs: Joi.array().items().optional(),
+  sideEffects: Joi.array().items().optional(),
+});
+
+export const getMedicationsSchema = Joi.object({
+  skip: Joi.number().optional(),
+  take: Joi.number().optional(),
+  where: Joi.string().optional(),
+  orderBy: Joi.string().optional(),
+});
