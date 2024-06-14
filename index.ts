98982714878468f25a552d74d8fe437901fd4669
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-14 15:36:30 +0100

    Feat: add payment route to api entry point

--- index.ts
+++ index.ts
@@ -19,6 +19,7 @@ import userRouter from "./features/user/route";
 import drugRouter from "./features/drug/route";
 import medicationRouter from "./features/medication/route";
 import appointmentRouter from "./features/appointment/route";
+import paymentRouter from "./features/payment/route";
 import swaggerRouter from "./swagger";
 
 dotenv.config();
@@ -43,6 +44,7 @@ app.use("/api/user", userRouter);
 app.use("/api/drug", drugRouter);
 app.use("/api/medication", medicationRouter);
 app.use("/api/appointment", appointmentRouter);
+app.use("/api/payment", paymentRouter);
 app.use("/api-docs", swaggerRouter);
 
 app.use(useNotFound);
